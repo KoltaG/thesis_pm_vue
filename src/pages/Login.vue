da1060bd056a1e2f705059916f3b1b8343cbbd97
Balázs <koltaibalazs1999@gmail.com>
2024-09-28 19:57:16 +0200

    Pretteer formatter

--- src/pages/Login.vue
+++ src/pages/Login.vue
@@ -4,13 +4,26 @@
       <h1 class="text-2xl font-bold text-center mb-6 text-gray-800">Login</h1>
 
       <form @submit.prevent="onSubmit">
-        <CustomInput v-model:modelValue="emailValue" label="Email" placeholder="Enter your email" :error="emailError" />
+        <CustomInput
+          v-model:modelValue="emailValue"
+          label="Email"
+          placeholder="Enter your email"
+          :error="emailError"
+        />
 
-        <CustomInput v-model:modelValue="passwordValue" type="password" label="Password"
-          placeholder="Enter your password" :error="passwordError" />
+        <CustomInput
+          v-model:modelValue="passwordValue"
+          type="password"
+          label="Password"
+          placeholder="Enter your password"
+          :error="passwordError"
+        />
 
-        <Button type="submit" :disabled="isSubmitting"
-          class="justify-center !text-center w-full p-3 bg-blue-500 text-white rounded-lg focus:ring-opacity-50 disabled:opacity-50">
+        <Button
+          type="submit"
+          :disabled="isSubmitting"
+          class="justify-center !text-center w-full p-3 bg-blue-500 text-white rounded-lg focus:ring-opacity-50 disabled:opacity-50"
+        >
           Login
         </Button>
       </form>
@@ -19,25 +32,27 @@
 </template>
 
 <script setup lang="ts">
-import { useRouter } from 'vue-router';
-import { useAuth } from '../composables/useAuth';
-import { useForm, useField } from 'vee-validate';
-import * as yup from 'yup';
-import { toast } from 'vue3-toastify';
-import CustomInput from '../components/common/CustomInput.vue';
-import Button from '../components/common/Button.vue';
+import { useRouter } from "vue-router";
+import { useAuth } from "../composables/useAuth";
+import { useForm, useField } from "vee-validate";
+import * as yup from "yup";
+import { toast } from "vue3-toastify";
+import CustomInput from "../components/common/CustomInput.vue";
+import Button from "../components/common/Button.vue";
 
 const schema = yup.object({
-  email: yup.string().email('Invalid email format!').required('Required!'),
-  password: yup.string().required('Required!'),
+  email: yup.string().email("Invalid email format!").required("Required!"),
+  password: yup.string().required("Required!"),
 });
 
 const { handleSubmit, isSubmitting } = useForm({
   validationSchema: schema,
 });
 
-const { value: emailValue, errorMessage: emailError } = useField<string>('email');
-const { value: passwordValue, errorMessage: passwordError } = useField<string>('password');
+const { value: emailValue, errorMessage: emailError } =
+  useField<string>("email");
+const { value: passwordValue, errorMessage: passwordError } =
+  useField<string>("password");
 
 const { tryLogin } = useAuth();
 const router = useRouter();
@@ -45,10 +60,10 @@ const router = useRouter();
 const onSubmit = handleSubmit(async (values) => {
   try {
     await tryLogin(values.email, values.password);
-    toast.success('Login successful!');
-    router.push('/');
+    toast.success("Login successful!");
+    router.push("/");
   } catch (error) {
-    toast.error('Error logging in, please try again!');
+    toast.error("Error logging in, please try again!");
     console.error(error);
   }
 });
