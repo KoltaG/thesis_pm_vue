29f7d20b61c40c813a05f4f563ac4aa9b8900811
Balázs <koltaibalazs1999@gmail.com>
2024-10-06 22:07:00 +0200

    Add toast

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,18 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "./scss/global.scss";
+import "vue3-toastify/dist/index.css";
 import router from "./router/router";
+import Vue3Toastify, { ToastContainerOptions } from "vue3-toastify";
 
-createApp(App).use(router).mount("#app");
+const app = createApp(App);
+
+app.use(router);
+
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+  position: "top-right",
+  pauseOnHover: true,
+} as ToastContainerOptions);
+
+app.mount("#app");
